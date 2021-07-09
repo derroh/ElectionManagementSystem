0c4cca84bb42c7e9fb53e6e8df97187dcc369be0
Derrick <derrickwitness@gmail.com>
2021-07-09 10:01:28 +0300

    Added student profile view

---
+++ Areas/Student/Controllers/VoteController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ElectionManagementSystem.Areas.Student.Controllers
+{
+    public class VoteController : Controller
+    {
+        // GET: Student/Vote
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
